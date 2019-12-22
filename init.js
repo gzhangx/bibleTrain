0f23f85b4461ea432b0bff77e540649b7be1af3b
gang zhang <gang.zhang@pps.io>
2019-12-22 15:18:03 -0500

    test

--- init.js
+++ init.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const mapValues = require('lodash/mapValues');
+const allChapters = require('./chapters');
 
 function addSpace(l) {
     return l.split('').map(c=>{
@@ -16,7 +18,7 @@ function doSplit(line, splitChar) {
 function loadData(tag = 'cmn2006', splitChar='') {
     const dir = `./data/${tag}_readaloud`;
     const files = fs.readdirSync(dir).filter(f=>f.startsWith(tag) && !f.startsWith(`${tag}_000`));
-    console.log(files);
+    //console.log(files);
     fs.writeFileSync('chapters.json', JSON.stringify(files.map(f=>f.substr(tag.length+1).replace('_read.txt',''))))
     const allRes = files.reduce((acc,filename)=>{
         const fullName = `${dir}/${filename}`;
@@ -35,8 +37,28 @@ function loadData(tag = 'cmn2006', splitChar='') {
             return acc;
         }, acc);
     },{});
-    console.log(allRes);
+    console.log(Object.values(mapValues(allRes, (val,key)=>({word:key, count:val}))).sort((a,b)=>b.count-a.count));
 }
 
+
 //loadData();
-loadData('engnet', ' ');
+//loadData('engnet', ' ');
+
+
+function doChapter(chapter) {
+    const getFileName = tag=>`./data/${tag}_readaloud/${tag}_${chapter}_read.txt`;
+    const process = tag=>{
+        const fullName = getFileName(tag);
+        const splitChar = tag === 'cmn2006'?'':' ';
+        const lines = fs.readFileSync(fullName).toString().split('\n').map(l=>l.trim()).filter(x=>x);
+        return lines.reduce((acc, l)=>{
+            const chars = doSplit(l, splitChar);//  l.split(splitChar);
+            acc.push(chars);
+            return acc;
+        }, []);
+    };
+    //console.log(process('cmn2006'));
+    console.log(process('engnet'));
+}
+
+doChapter('002_GEN_01');
